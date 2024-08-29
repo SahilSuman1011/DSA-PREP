1f20355daeafdacf414dd8f4096795331e3ffb28
Sahil Suman <121573166+SahilSuman1011@users.noreply.github.com>
2024-08-30 03:12:23 +0530

    Time: 192 ms (5.38%) | Memory: 18 MB (92.07%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,32 +1,31 @@
 class Solution {
-private:
-    int connectedComponentCount = 0;
-
-    int findRepresentative(int element, vector<int>& setRepresentatives) {
-        if (setRepresentatives[element] == 0) {
-            setRepresentatives[element] = element;
-            connectedComponentCount++;
-        }
-        return setRepresentatives[element] == element ? element : 
-               (setRepresentatives[element] = findRepresentative(setRepresentatives[element], setRepresentatives));
-    }
-
-    void mergeComponents(int elementA, int elementB, vector<int>& setRepresentatives) {
-        int repA = findRepresentative(elementA, setRepresentatives);
-        int repB = findRepresentative(elementB, setRepresentatives);
-        if (repA != repB) {
-            setRepresentatives[repB] = repA;
-            connectedComponentCount--;
-        }
-    }
-
 public:
-    int removeStones(vector<vector<int>>& stonePositions) {
-        vector<int> setRepresentatives(20003, 0);
-        for (const auto& stonePosition : stonePositions) {
-            mergeComponents(stonePosition[0] + 1, stonePosition[1] + 10002, setRepresentatives);
+    bool isValid(vector<int>&pos1, vector<int>&pos2)
+    {
+        if (pos1[0] == pos2[0]) return true;
+        if (pos1[1] == pos2[1]) return true;
+        return false;
+    }
+    void dfs(vector<vector<int>>&stones, vector<bool>&visited, int currIdx)
+    {
+        visited[currIdx] = true;
+        for (int i = 0; i < stones.size(); i++)
+        {
+            if (visited[i]) continue;
+            if (isValid(stones[i], stones[currIdx]))  dfs(stones, visited, i);
         }
-        return stonePositions.size() - connectedComponentCount;
+    }
+    int removeStones(vector<vector<int>>& stones)
+    {
+        int n = stones.size();
+        vector<bool>visited(n, false);
+        int componentCount = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[i]) continue;
+            dfs(stones, visited, i);
+            componentCount++;
+        }
+        return (n - componentCount); 
     }
 };
-
